a6a8577d944fcb60e3c2c3c44b3f97e3c6485379
Christian Asch <casch@cenat.ac.cr>
2023-09-02 06:04:00 -0600

    Fixed configured file

--- src/main_program/src/main.cpp
+++ src/main_program/src/main.cpp
@@ -1,9 +1,11 @@
 #include <CLI/CLI.hpp>
+#include <spdlog/spdlog.h>
+#include <config.hpp>
 
 int main(int argc, char ** argv)
 {
   CLI::App app("We're doing a raytracer");
-
   CLI11_PARSE(app, argc, argv)
+  spdlog::info("Ray version: {}", ray::info::project_version);
 
 }
